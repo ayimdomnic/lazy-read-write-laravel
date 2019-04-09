11b8404bb61f1a47f47db952f093c93274d0ed6f
ayimdomnic <ayimdomnic@gmail.com>
2019-04-09 15:12:14 +0300

    test connectors all working with laravel 5.8

--- src/Logic/Connector.php
+++ src/Logic/Connector.php
@@ -1,7 +1,11 @@
 <?php
 
 
-class Connector
+namespace Ayimdomnic\LazySql\Logic;
+
+use Illuminate\Database\Connectors\MySqlConnector as MySqlConnectorAlias;
+
+class Connector extends MySqlConnectorAlias
 {
 
 }
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,8 +3,19 @@
 
 namespace Ayimdomnic\LazySql;
 
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider  extends BaseServiceProvider
 {
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
 
 }
